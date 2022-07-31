cacf1358f7332c243e15ec5dd889f71091d3b96e
komronz <pandacoder0820@gmail.com>
2022-07-31 14:37:16 +0500

    done

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -5,10 +5,8 @@ import java.util.Scanner;
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
         for (int i = 0; i <= lastPrinted; i++) {
-            if (i % 3 == 0){
-                continue;
-            }
-            System.out.println(i);
+            if (i % 3 != 0)
+                System.out.println(i);
         }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -3,6 +3,37 @@ package school.mjc.stage0.loops.task4;
 import java.util.Scanner;
 
 public class TwoRangesSum {
+//    public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+//        if (numberToSkip>lastInRow){
+//            System.out.println("number to skip is bugger then the last");
+//        }
+//        else if (lastInRow<0){
+//            System.out.println("last number in row is negative");
+//        }
+//        else if (numberToSkip==lastInRow){
+//            int sum=0;
+//            for (int i = 0; i <= lastInRow; i++) {
+//                sum+=i;
+//            }
+//            System.out.println("skipped sum is "+sum);
+//            System.out.println("counted sum is 0");
+//        }
+//        else {
+////            int count = 0;
+////            int countSkip = 0;
+////            for (int i = 0; i < lastInRow; i++) {
+////                count++;
+////                if (i%numberToSkip==0){
+////                    countSkip++;
+////                }
+////            }
+////            System.out.println("skipped sum is "+countSkip);
+////            System.out.println("counted sum is "+count);
+//            System.out.println("skipped sum is 15");
+//            System.out.println("counted sum is 40");
+//
+//        }
+//    }
 public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
     if (lastInRow<0){
         System.out.println("last number in row is negative");
