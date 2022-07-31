15c12d8b9fc6d3b11f917705254db6bad990d88a
komronz <pandacoder0820@gmail.com>
2022-07-31 14:30:24 +0500

    done

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -1,5 +1,7 @@
 package school.mjc.stage0.loops.task4;
 
+import java.util.Scanner;
+
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
         for (int i = 0; i <= lastPrinted; i++) {
