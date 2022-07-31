4dfb5328d00263c0aabcde2cce142df6f2dbe5cd
komronz <pandacoder0820@gmail.com>
2022-07-31 14:32:48 +0500

    done

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -4,26 +4,19 @@ import java.util.Scanner;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
-//        if (toBreakWith>=numberToGoUntil){
-//            System.out.println("iterating till the end");
-//            for (int i = 1; i <= numberToGoUntil; i++) {
-//                System.out.println(i);
-//            }
-//        }
-//        else {
-//            for (int i = 1; i <= numberToGoUntil; i++) {
-//                System.out.println(i);
-//                if (i==toBreakWith){
-//                    break;
-//                }
-//            }
-//        }
-        if (toBreakWith>=numberToGoUntil)
+        if (toBreakWith>=numberToGoUntil){
             System.out.println("iterating till the end");
-        for (int i = 1; i <= numberToGoUntil; i++) {
-            System.out.println(i);
-            if (i==toBreakWith)
-                break;
+            for (int i = 1; i <= numberToGoUntil; i++) {
+                System.out.println(i);
+            }
+        }
+        else {
+            for (int i = 1; i <= numberToGoUntil; i++) {
+                System.out.println(i);
+                if (i==toBreakWith){
+                    break;
+                }
+            }
         }
     }
 
